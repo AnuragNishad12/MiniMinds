ab7d73574afca53b21649c0f6d4f165ceb57f192
Anurag Nishad <124987038+AnuragNishad12@users.noreply.github.com>
2024-09-03 13:27:32 +0530

    Some Changes In Bear.dart

--- lib/EnglishComponents/Animals/EnglishComponents/Animals/AnimalDetailsPage/AnimalsPage/Bears.dart
+++ lib/EnglishComponents/Animals/EnglishComponents/Animals/AnimalDetailsPage/AnimalsPage/Bears.dart
@@ -5,9 +5,6 @@ class BlackBears extends StatefulWidget {
   const BlackBears({super.key});
 
 
-
-
-
   @override
   State<BlackBears> createState() => _BlackBearsState();
 }
@@ -33,7 +30,62 @@ class _BlackBearsState extends State<BlackBears> {
             ),
           ),
         ),
-      // body: ,
+      body:Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(padding: const EdgeInsets.all(16.0),
+            child:GestureDetector(
+              onTap: (){},
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.yellow,
+                  borderRadius: BorderRadius.circular(30),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.orange,
+                      offset: Offset(4, 4),
+                      blurRadius: 10,
+                    ),
+                  ],
+                  gradient: LinearGradient(
+                    colors: [Colors.orange, Colors.red],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                  ),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min, // Ensure Row's width is just enough for its children
+                  children: [
+                    Icon(
+                      Icons.star,
+                      color: Colors.white,
+                      size: 24,
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      'Click Me!',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontFamily: 'Comic Sans MS',
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Icon(
+                      Icons.star,
+                      color: Colors.white,
+                      size: 24,
+                    ),
+                  ],
+                ),
+              ) ,
+            )
+    ),
+        ],
+      )
+
     );
   }
 }
