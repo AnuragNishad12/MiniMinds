d4aa4ba92cdb271c0c5abed7556308c1ff7207fd
Anurag Nishad <124987038+AnuragNishad12@users.noreply.github.com>
2024-09-04 12:48:17 +0530

    Completed the ar buuton part in Bears

--- lib/EnglishComponents/Animals/EnglishComponents/Animals/AnimalDetailsPage/AnimalsPage/Bears.dart
+++ lib/EnglishComponents/Animals/EnglishComponents/Animals/AnimalDetailsPage/AnimalsPage/Bears.dart
@@ -31,10 +31,7 @@ class _BlackBearsState extends State<BlackBears> {
         return AlertDialog(
           content: SizedBox(
             height: 400.0, // Specify a height for the image slider
-            width: MediaQuery
-                .of(context)
-                .size
-                .width * 0.8, // Specify a width for the image slider
+            width: MediaQuery.of(context).size.width * 0.8, // Specify a width for the image slider
             child: ImageSlider(imageUrls: _imageUrls),
           ),
         );
@@ -43,8 +40,10 @@ class _BlackBearsState extends State<BlackBears> {
   }
 
   Future<List<String>> _getImageUrls() async {
-    ListResult result = await FirebaseStorage.instance.ref().child(
-        'Animals/Bears/BearsSlider').listAll();
+    ListResult result = await FirebaseStorage.instance
+        .ref()
+        .child('Animals/Bears/BearsSlider')
+        .listAll();
     List<String> imageUrls = [];
 
     for (var ref in result.items) {
@@ -75,101 +74,134 @@ class _BlackBearsState extends State<BlackBears> {
           ),
         ),
       ),
-      body: Column(
+      body: Stack(
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+          Column(
             children: [
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: GestureDetector(
-                  onTap: _showImageSlider,
-                  // Call the method to show the image slider
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.yellow,
-                      borderRadius: BorderRadius.circular(30),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.orange,
-                          offset: Offset(4, 4),
-                          blurRadius: 10,
-                        ),
-                      ],
-                      gradient: LinearGradient(
-                        colors: [Colors.orange, Colors.red],
-                        begin: Alignment.topLeft,
-                        end: Alignment.bottomRight,
-                      ),
-                    ),
-                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Icon(
-                          Icons.star,
-                          color: Colors.white,
-                          size: 24,
-                        ),
-                        SizedBox(width: 10),
-                        Text(
-                          'Click Me!',
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                            fontFamily: 'Comic Sans MS',
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: GestureDetector(
+                      onTap: _showImageSlider,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.yellow,
+                          borderRadius: BorderRadius.circular(30),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.orange,
+                              offset: Offset(4, 4),
+                              blurRadius: 10,
+                            ),
+                          ],
+                          gradient: LinearGradient(
+                            colors: [Colors.orange, Colors.red],
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
                           ),
                         ),
-                        SizedBox(width: 10),
-                        Icon(
-                          Icons.star,
-                          color: Colors.white,
-                          size: 24,
+                        padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Icon(
+                              Icons.star,
+                              color: Colors.white,
+                              size: 24,
+                            ),
+                            SizedBox(width: 10),
+                            Text(
+                              'Click Me!',
+                              style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                fontFamily: 'Comic Sans MS',
+                              ),
+                            ),
+                            SizedBox(width: 10),
+                            Icon(
+                              Icons.star,
+                              color: Colors.white,
+                              size: 24,
+                            ),
+                          ],
                         ),
-                      ],
+                      ),
                     ),
                   ),
-                ),
+                ],
               ),
+              if (_isLoading)
+                CircularProgressIndicator(),
             ],
           ),
-          if (_isLoading)
-            CircularProgressIndicator(),
-          // Show a loading indicator while fetching images
+          Positioned(
+            bottom: 20,
+            right: 20,
+            child:InkWell(
+              onTap: (){},
+            child: Container(
+                width: 100.0,
+                height: 100.0,
+                padding: EdgeInsets.all(8.0), // Add padding inside the container
+                decoration: BoxDecoration(
+                color: Color(0x80BEBEBE), // 50% transparent grey background
+                borderRadius: BorderRadius.circular(15.0), // Add border radius
+                border: Border.all(
+               color: Colors.orange, // Orange border color
+             width: 2.0, // Border width
+                 ),
+                ),
+                child:Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/image/ArGlass.png',
+                      width: 60,
+                      height: 60,
+                      fit: BoxFit.contain,
+                    ),
+                    Text("Ar View",style: TextStyle(fontSize: 10,fontWeight: FontWeight.bold),)
+                  ],
+                )
+            ),
+            ),
+          ),
         ],
       ),
     );
   }
 }
 
-
-  class ImageSlider extends StatelessWidget {
+class ImageSlider extends StatelessWidget {
   final List<String> imageUrls;
 
   ImageSlider({required this.imageUrls});
 
   @override
   Widget build(BuildContext context) {
-  return CarouselSlider(
-  options: CarouselOptions(
-  height: 300.0, // Ensure a fixed height for each image in the slider
-  viewportFraction: 1.0, // Adjust this to change the image size in the viewport
-  ),
-  items: imageUrls.map((url) {
-  return Builder(
-  builder: (BuildContext context) {
-  return Container(
-  width: MediaQuery.of(context).size.width,
-  margin: EdgeInsets.symmetric(horizontal: 5.0),
-  decoration: BoxDecoration(
-  // color: Colors.amber,
-  ),
-  child: Image.network(url, fit: BoxFit.cover),
-  );
-  },
-  );
-  }).toList(),
-  );
+    return CarouselSlider(
+      options: CarouselOptions(
+        height: 300.0, // Ensure a fixed height for each image in the slider
+        viewportFraction: 1.0, // Adjust this to change the image size in the viewport
+      ),
+      items: imageUrls.map((url) {
+        return Builder(
+          builder: (BuildContext context) {
+            return Container(
+              width: MediaQuery.of(context).size.width,
+              margin: EdgeInsets.symmetric(horizontal: 5.0),
+              decoration: BoxDecoration(
+                // Optional: Add decoration if needed
+              ),
+              child: Image.network(url, fit: BoxFit.cover),
+            );
+          },
+        );
+      }).toList(),
+    );
   }
-  }
+}
